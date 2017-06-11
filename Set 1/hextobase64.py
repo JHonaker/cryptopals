681e3fed419d31c6d8de0a9045167e01512a3319
JHonaker <honaker215@gmail.com>
2017-06-11 12:13:11 -0400

    Completed Hex to Base 64 conversion.

---
+++ Set 1/hextobase64.py
@@ -0,0 +1,19 @@
+# Challenge 1: Convert hex to base 64.
+
+# The string:
+# 49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d
+# should produce:
+# SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t
+
+import codecs
+
+
+def convert(encoded_hex):
+    # Drop the newline from the Base64 encoded string
+    return codecs.encode(codecs.decode(encoded_hex, "hex"), "base64")[:-1]
+
+
+if __name__ == "__main__":
+    test_hex = b'49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d'
+    test_base64 = b'SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t'
+    assert convert(test_hex) == test_base64
